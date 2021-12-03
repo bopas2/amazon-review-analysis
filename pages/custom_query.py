f0747cdf329c80a1eee97df404101b841883d26b
Christina <czhao43@gmail.com>
2021-12-03 11:33:39 -0500

    create dummy network and show

--- pages/custom_query.py
+++ pages/custom_query.py
@@ -3,6 +3,7 @@ import streamlit.components.v1 as components
 import pandas as pd
 import networkx as nx
 from pyvis.network import Network
+from pages import make_network
 
 # set header title
 def app():
@@ -16,3 +17,8 @@ def app():
         st.warning("Please enter a custom query")
     else:
         st.write("Your query is: ", query)
+        make_network.make_network(query)
+
+        HtmlFile = open('nodes.html','r',encoding='utf-8')
+        source_code = HtmlFile.read()
+        components.html(source_code, height = 1200, width = 1200)
---
+++ pages/make_network.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import pandas as pd
+import networkx as nx
+from pyvis.network import Network
+from pages import AmazonViz
+
+def make_network(query = None):
+    AmazonViz.create(query)
